b9984f3d2cf4380aad4982494b5b8e5bb9fbcafc
Paolo Panarese <panarese.paolo@gmail.com>
2024-10-15 20:25:52 +0200

    skip stt message if no audio

--- src/stt.rs
+++ src/stt.rs
@@ -47,11 +47,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
         log::debug!("{}: {:?}", topic, message);
         match topic.as_str() {
             STT_TOPIC => {
-                module.send_event(MODULE_NAME, STT_STARTED_EVENT, &Message::default()).await?;
-                let stt_manager = get_stt(&mut module)?.expect("Error loading STT module");
-                let response_text = stt_manager.convert(message.text.clone()).await.map_err(|e| e.to_string())?;
-                module.send_event(MODULE_NAME, STT_ENDED_EVENT, &Message::default()).await?;
-                let (response_topic, response) = message.reply(response_text, MessageType::TEXT).expect("Error on create response");
+                let (response_text, response_type) = match message.message_type {
+                    MessageType::AUDIO => {
+                        module.send_event(MODULE_NAME, STT_STARTED_EVENT, &Message::default()).await?;
+                        let stt_manager = get_stt(&mut module)?.expect("Error loading STT module");
+                        let response_text = stt_manager.convert(message.text.clone()).await.map_err(|e| e.to_string())?;
+                        module.send_event(MODULE_NAME, STT_ENDED_EVENT, &Message::default()).await?;
+                        (response_text, MessageType::TEXT)
+                    }
+                    _ => {
+                        (message.text.clone(), message.message_type)
+                    }
+                };
+                let (response_topic, response) = message.reply(response_text, response_type).expect("Error on create response");
                 module.send(&response_topic, &response).await.expect("Error on publish");
             },
             _ => {}
